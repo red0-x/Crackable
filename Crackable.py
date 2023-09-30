90cfcdec0e102e1b11747dccdbb49cd380a6996c
red0xx <83562024+red0-x@users.noreply.github.com>
2023-09-30 12:41:16 -0700

    asdfads
    fasdf

--- Crackable.py
+++ Crackable.py
@@ -10,8 +10,8 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/hello/", response_class=HTMLResponse)
 
-async def main(request: Request):
-   return templates.TemplateResponse("Crackable.html", {"request": request})
+async def app(request: Request):
+    return templates.TemplateResponse("Crackable.html", {"request": request})
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", port=5000, log_level="info")
+    uvicorn.run("Crackable:app", port=5000, log_level="info")
