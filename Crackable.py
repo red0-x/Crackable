84e4df8e5c482304053293418f01c208ac5a7f8d
red0xx <83562024+red0-x@users.noreply.github.com>
2023-09-30 12:39:11 -0700

    Update Crackable.py

--- Crackable.py
+++ Crackable.py
@@ -1,1 +1,17 @@
 #Crackable? AI Password Analyzer.
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi import FastAPI, Request
+import uvicorn
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/hello/", response_class=HTMLResponse)
+
+async def main(request: Request):
+   return templates.TemplateResponse("Crackable.html", {"request": request})
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=5000, log_level="info")
