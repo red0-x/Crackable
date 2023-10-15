ffe13f8fec605f895caf25e160789b2b060000ba
red0xx <83562024+red0-x@users.noreply.github.com>
2023-10-14 20:02:03 -0700

    update

--- Crackable.py
+++ Crackable.py
@@ -1,6 +1,6 @@
 #Crackable? AI Password Analyzer.
 
-from flask import Flask,render_template, request
+from flask import Flask,render_template, request, redirect
 
 from utils import *
 
@@ -14,11 +14,36 @@ app = Flask(__name__)
 @app.route('/', methods =["GET", "POST"])
 
 
-   
 def CrackableMain(methods=['GET']):
  if request.method == "POST":
     password = request.form.get("Password")
+    hashes = request.form.get("dropdown")
+    
+    modea = request.form.get("modea")
+    print(modea)
+    if modea == 'True':
+      redirect('/advanced')
+    else:
+    
+      u = utility()
+      #  u.cracktime(crackpassword=password, hps=  hashes)
+      response = u.cracktime(crackpassword=password, hps=hashes)
+      print(response)
+      return str(response)
+  
+ 
+ return render_template("index.html")
+
+ 
+
+@app.route('/advanced', methods =["GET", "POST"])
+
+
+def Advanced(methods=['GET']):
+ if request.method == "POST":
+    password = request.form.get("Password")
     hashes = request.form.get("Hashes")
+    
     u = utility()
    #  u.cracktime(crackpassword=password, hps=  hashes)
     response = u.cracktime(crackpassword=password, hps=hashes)
@@ -26,7 +51,8 @@ def CrackableMain(methods=['GET']):
     return str(response)
 
 
- return render_template("index.html")
+
+ return render_template("advanced.html")
 
  
 
