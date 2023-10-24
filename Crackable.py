24db7fd230af41dc1ec65cb77f4df3cf232fec80
red0xx <83562024+red0-x@users.noreply.github.com>
2023-10-23 18:55:24 -0700

    asd
    asd

--- Crackable.py
+++ Crackable.py
@@ -1,8 +1,7 @@
 from flask import Flask, render_template, request, flash
 import os
+from utils import utility  # Import the utility class or module
 from dotenv import load_dotenv
-from utils import utility
-
 load_dotenv()
 
 SECRET_KEY = os.getenv('SECRET_KEY')
@@ -10,36 +9,38 @@ app = Flask(__name__)
 app.secret_key = SECRET_KEY
 
 @app.route('/', methods=['GET', 'POST'])
-def crackable_main():
+def CrackableMain():
     if request.method == "POST":
         password = request.form.get("Password")
         hashes = request.form.get("Dropdown")
 
-        u = utility()
+        u = utility()  # You need to have the utility class defined
         response = u.cracktime(crackpassword=password, hps=hashes)
         parsed = response.split(" ")
         cracked = parsed[0]
         time = parsed[1]
-        try:
-            crackfast = parsed[2]
-        except Exception:
-            print('no crackfast! password insecure!')
+        crackfast = None  # Initialize crackfast
+
+        if len(parsed) > 2:
+            try:
+                crackfast = parsed[2]
+            except ValueError:
+                print('no crackfast! password insecure')
 
         if response:
             print(response)
-        
-        crackable = f'Crackable! Your password could be cracked in {cracked} {time}! I recommend generating a new password!'
-        secure = f'Good Job! Your password is secure and could be cracked in {cracked} {time}!'
 
         if crackfast is None:
-            return render_template('secure.html', response=secure)
+            return render_template('secure.html', response=f'Good Job! Your password is secure and could be cracked in {cracked} {time}!')
         else:
-            return render_template('cracked.html', response=crackable)
+            return render_template('cracked.html', response=f'Crackable! Your password could be cracked in {cracked} {time}! I recommend generating a new password!')
 
     return render_template("index.html")
 
+
 @app.route('/password-generator', methods=["POST", "GET"])
-def password_gen():
+
+def PasswordGen():
     if request.method == "POST":
         passwordlength = int(request.form['length'])
         lchars = 'lchars' in request.form
@@ -51,31 +52,33 @@ def password_gen():
             flash("Invalid input. Please enter a valid length.", 'error')
             return render_template('password-generator.html')
 
-        generated_password = utility.GeneratePassword(passwordlength, lchars, uchars, nums, symbols)  # The arguments should not have 'passwordlength=', etc.
+        generated_password = utility.GeneratePassword(passwordlength, lchars, uchars, nums, symbols)
         flash("Password generated successfully!", 'success')
         return render_template('password-generator.html', output=generated_password)
-    return render_template('password-generator.html')
-    
 
-@app.route('/advanced', methods=["GET", "POST"])
-def advanced():
+
+@app.route('/Advanced', methods=["GET", "POST"])
+
+def Advanced():
     if request.method == "POST":
         password = request.form.get("Password")
         hashes = request.form.get("Dropdown")
-        try:
-            hashes = request.form.get("Hashes")
-        except Exception:
+        if "Hashes" not in request.form:
             print('NO Hashes!')
+            hashes = None
 
         u = utility()
         response = u.cracktime(crackpassword=password, hps=hashes)
         parsed = response.split(" ")
         cracked = parsed[0]
         time = parsed[1]
-        try:
-            crackfast = parsed[2]
-        except Exception:
-            print('no crackfast! password insecure!')
+        crackfast = None  # Initialize crackfast
+
+        if len(parsed) > 2:
+            try:
+                crackfast = parsed[2]
+            except Exception:
+                print('no crackfast! password insecure')
 
         if response:
             print(response)
@@ -89,6 +92,10 @@ def advanced():
             return render_template('cracked.html', response=crackable)
 
     return render_template("advanced.html")
+
+
+
+app.debug = True
 
 if __name__ == '__main__':
     app.run()
