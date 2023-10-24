4e341a9256db285e533a1d1abdfb375520234487
red0xx <83562024+red0-x@users.noreply.github.com>
2023-10-23 17:05:22 -0700

    sdf
    sdf

--- Crackable.py
+++ Crackable.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, flash
+from flask import Flask, render_template, request, flash
 import os
 from dotenv import load_dotenv
 from utils import utility
@@ -9,30 +9,37 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 app = Flask(__name__)
 app.secret_key = SECRET_KEY
 
-@app.route('/', methods=["GET", "POST"])
-def CrackableMain():
+@app.route('/', methods=['GET', 'POST'])
+def crackable_main():
     if request.method == "POST":
         password = request.form.get("Password")
-        hashes = request.form.get("dropdown")
+        hashes = request.form.get("Dropdown")
 
-        if password is None:
-            flash("Invalid input. Please enter a password.", 'error')
-            return redirect(request.url)
-
-        if hashes is None:
-            flash("Invalid input. Please select a hash type.", 'error')
-            return redirect(request.url)
-
+        u = utility()
+        response = u.cracktime(crackpassword=password, hps=hashes)
+        parsed = response.split(" ")
+        cracked = parsed[0]
+        time = parsed[1]
         try:
-            hashes = int(hashes)
-        except ValueError:
-            flash("Invalid input. Please select a valid hash type.", 'error')
-            return redirect(request.url)
+            crackfast = parsed[2]
+        except Exception:
+            print('no crackfast! password insecure!')
 
-    return render_template('index.html')
+        if response:
+            print(response)
+        
+        crackable = f'Crackable! Your password could be cracked in {cracked} {time}! I recommend generating a new password!'
+        secure = f'Good Job! Your password is secure and could be cracked in {cracked} {time}!'
+
+        if crackfast is None:
+            return render_template('secure.html', response=secure)
+        else:
+            return render_template('cracked.html', response=crackable)
+
+    return render_template("index.html")
 
 @app.route('/password-generator', methods=["POST", "GET"])
-def PasswordGen():
+def password_gen():
     if request.method == "POST":
         passwordlength = int(request.form['length'])
         lchars = 'lchars' in request.form
@@ -44,31 +51,44 @@ def PasswordGen():
             flash("Invalid input. Please enter a valid length.", 'error')
             return render_template('password-generator.html')
 
-        generated_password = utility.GeneratePassword(passwordlength=passwordlength, lchars=lchars, uchars=uchars, nums=nums, symbols=symbols)
+        generated_password = utility.GeneratePassword(passwordlength, lchars, uchars, nums, symbols)  # The arguments should not have 'passwordlength=', etc.
         flash("Password generated successfully!", 'success')
         return render_template('password-generator.html', output=generated_password)
+    return render_template('password-generator.html')
+    
 
-@app.route('/Advanced', methods=["GET", "POST"])
-def Advanced():
+@app.route('/advanced', methods=["GET", "POST"])
+def advanced():
     if request.method == "POST":
         password = request.form.get("Password")
-        hashes = request.form.get("Hash")
-
-        if password is None:
-            flash("Invalid input. Please enter a password.", 'error')
-            return redirect(request.url)
-
-        if hashes is None:
-            flash("Invalid input. Please select a hash type.", 'error')
-            return redirect(request.url)
-
+        hashes = request.form.get("Dropdown")
         try:
-            hashes = int(hashes)
-        except ValueError:
-            flash("Invalid input. Please select a valid hash type.", 'error')
-            return redirect(request.url)
+            hashes = request.form.get("Hashes")
+        except Exception:
+            print('NO Hashes!')
 
-    return render_template('advanced.html')
+        u = utility()
+        response = u.cracktime(crackpassword=password, hps=hashes)
+        parsed = response.split(" ")
+        cracked = parsed[0]
+        time = parsed[1]
+        try:
+            crackfast = parsed[2]
+        except Exception:
+            print('no crackfast! password insecure!')
+
+        if response:
+            print(response)
+        
+        crackable = f'Crackable! Your password could be cracked in {cracked} {time}! I recommend generating a new password!'
+        secure = f'Good Job! Your password is secure and could be cracked in {cracked} {time}!'
+
+        if crackfast is None:
+            return render_template('secure.html', response=secure)
+        else:
+            return render_template('cracked.html', response=crackable)
+
+    return render_template("advanced.html")
 
 if __name__ == '__main__':
     app.run()
