2228d2903315c638f83f067623d33bc8b79fcbf1
red0xx <83562024+red0-x@users.noreply.github.com>
2026-05-03 12:10:15 -0700

    Add Util fixes.
    Cleaned up for clarity, better quality as well.
    
    - Still super slow due to being written in python

--- utils.py
+++ utils.py
@@ -1,169 +1,88 @@
-from getpass import getpass
-import sys
 import re
-import random 
+import random
+
 
 class utility:
- 
+
     def __init__(self) -> None:
-         pass
+        pass
 
-    
-    
+    def cracktime(self, crackpassword, hps):
+        entropy = 0
+        crack_speed = int(hps)
 
+        policies = {
+            'Uppercase characters': 0,
+            'Lowercase characters': 0,
+            'Special characters': 0,
+            'Numbers': 0,
+        }
 
-    def cracktime(self,crackpassword,hps):
-            
-            # Analyze the password  and get the time to crack the user's password
-            entropy = 0
-           
-            crack_speed = int(hps) # Hashes Per second inputted by user
+        entropies = {
+            'Uppercase characters': 26,
+            'Lowercase characters': 26,
+            'Special characters': 33,
+            'Numbers': 10,
+        }
 
-            if len(sys.argv) > 1:
-                if sys.argv[1].isdigit():
-                    crack_speed = int(sys.argv[1])
+        password = str(crackpassword)
+        pass_len = len(password)
 
-            policies = {'Uppercase characters': 0,
-                        'Lowercase characters': 0,
-                        'Special characters': 0,
-                        'Numbers': 0}
-
-            entropies = {'Uppercase characters': 26,
-                        'Lowercase characters': 26,
-                        'Special characters': 33,
-                        'Numbers': 10}
-
-        
-
-            password = str(crackpassword)
-          
-            pass_len = len(password)
-
-            for char in password:
-
-                if re.match("[0-9]", char):
-                    policies["Numbers"] += 1
-
-                elif re.match("[a-z]", char):
-                    policies["Lowercase characters"] += 1
-
-                elif re.match("[A-Z]", char):
-                    policies["Uppercase characters"] += 1
-
-                else: # elif re.match("[\[\] !\"#$%&'()*+,-./:;<=>?@\\^_`{|}~]", char): # This regex can be used, but everything else should be considered special char
-                    policies["Special characters"] += 1
-
-            del password # Remove password from memory
-
-            
-
-            for policy in policies.keys():
-
-                num = policies[policy] if policies[policy] > 0 else '-' # Handle missing policies
-
-                if policies[policy] > 0:
-                    entropy += entropies[policy]
-
-        
-
-            # Calculate the time to crack
-            time_ = "hours"
-            cracked = ((entropy**pass_len) / crack_speed) / 60 # Hours in seconds
-
-            if cracked > 60:
-                cracked = cracked / 60
-                time_ = "minutes"
-                
-            if cracked > 24:
-                cracked = cracked / 24
-                time_ = "days"
-
-            if cracked > 365:
-                cracked = cracked / 365
-                time_ = "years"
-
-            if time_ == "years" and cracked > 100:
-                cracked = cracked / 100
-                time_ = "centuries"
-
-            if time_ == "centuries" and cracked > 1000:
-                cracked = cracked / 1000
-                time_ = "millennia"
-            
-        
-            
-            result1 = ("Time to crack password:   {:,.2f} {}".format(cracked, time_))
-            
-            
-            if time_ == "days":
-                crackfastt=True
-                print(cracked, time_, crackfastt)
-                return(str(f"{int(cracked,)} {(time_)} {crackfastt}"))   
-            
-            if time_ == "minutes":
-                crackfastt=True
-                print(cracked, time_, crackfastt)
-                return(str(f"{int(cracked,)} {(time_)} {crackfastt}"))  
-            
-            if time_ == "hours":
-                crackfastt=True
-                print(cracked, time_, crackfastt)
-                return(str(f"{int(cracked,)} {(time_)} {crackfastt}"))
+        for char in password:
+            if re.match("[0-9]", char):
+                policies["Numbers"] += 1
+            elif re.match("[a-z]", char):
+                policies["Lowercase characters"] += 1
+            elif re.match("[A-Z]", char):
+                policies["Uppercase characters"] += 1
             else:
-                 crackfastt=False
-                 return(str(f"{int(cracked,)} {(time_)} {crackfastt}"))
-                
+                policies["Special characters"] += 1
 
+        for policy in policies:
+            if policies[policy] > 0:
+                entropy += entropies[policy]
 
+        time_ = "seconds"
+        cracked = (entropy ** pass_len) / crack_speed
 
-            
+        if cracked > 60:
+            cracked /= 60
+            time_ = "minutes"
+        if cracked > 60:
+            cracked /= 60
+            time_ = "hours"
+        if cracked > 24:
+            cracked /= 24
+            time_ = "days"
+        if cracked > 365:
+            cracked /= 365
+            time_ = "years"
+        if time_ == "years" and cracked > 100:
+            cracked /= 100
+            time_ = "centuries"
+        if time_ == "centuries" and cracked > 1000:
+            cracked /= 1000
+            time_ = "millennia"
 
+        crackfastt = time_ in ("seconds", "minutes", "hours", "days")
+        return f"{int(cracked)} {time_} {crackfastt}"
 
-    def GeneratePassword(length,lchars,uchars,nums,symbols,self):
-            
-            uuchars = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-            llchars = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","q","y","z"]
-            nnums = ["1","2","3","4","5","6","7","8","9","0"]
-            ssymbols = ["!","@","$","#","?","&"]
-            
-            passwd = []
+    @staticmethod
+    def GeneratePassword(length, lchars, uchars, nums, symbols):
+        uuchars = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+        llchars = list("abcdefghijklmnopqrstuvwxyz")
+        nnums = list("1234567890")
+        ssymbols = list("!@$#?&")
 
-            total = []
+        total = []
 
-            if uchars == True:
+        if uchars:
+            total.extend(uuchars)
+        if lchars:
+            total.extend(llchars)
+        if nums:
+            total.extend(nnums)
+        if symbols:
+            total.extend(ssymbols)
 
-                total.extend(uuchars)
-                
-
-                
-            if lchars == True:
-
-                total.extend(llchars)
-
-            
-            if nums == True:
-
-                total.extend(nnums)
-
-                
-            
-            if symbols == True:
-
-                total.extend(ssymbols)
-                
-            x = [i for i in (total)]
-            random.shuffle(x)
-
-            for i in range(length):
-            
-                passwd += random.choices(x)[0]  
-                passwd = ''.join(list(passwd))
-                
-            return passwd
-
-            
-                
-            # GeneratePassword(length=11,lchars=True,uchars=True,nums=True,symbols=True)
-            # cracktime(crackpassword='asdfasdfaw',hps='1000000000')
-
-
+        return ''.join(random.choices(total, k=length))
